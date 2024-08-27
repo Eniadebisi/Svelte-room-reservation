6e10330a42fe9b28da18979d0957765a2b88a4bd
Eni Adebisi <eniadebisi@gmail.com>
2024-08-27 15:07:45 -0400

    bootstrap added websiteName global variable

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import 'bootstrap/dist/css/bootstrap.css';
+    import scriptSrc from 'bootstrap/dist/js/bootstrap.bundle.js?url';
+</script>
+<svelte:head>
+  <script src={scriptSrc}></script>
+</svelte:head>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+  import { websiteName } from "$lib/helper";
+
+</script>
+<div class="container text-center">
+    <h1>Welcome to {websiteName}</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+</div>
