6c4f41bf49d7c337d526a36a86ade211ed85cca6
Eni Adebisi <eniadebisi@gmail.com>
2024-08-28 06:57:38 -0400

    bootstrap icons installed restructered routing with login at head and added routes for logout and admin, profile. Writable to save user created (May change to user being stored in locals)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
     import 'bootstrap/dist/css/bootstrap.css';
     import scriptSrc from 'bootstrap/dist/js/bootstrap.bundle.js?url';
+    import "../scss/styles.scss"
+    import "bootstrap-icons/font/bootstrap-icons.css";
+
 </script>
 <svelte:head>
   <script src={scriptSrc}></script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,29 @@
-<script>
-
+<script lang="ts">
   import { websiteName } from "$lib/helper";
+  import type { PageData } from "./$types";
 
+  export let data: PageData;
+  let showPW = false;
+  function togglePW() {
+    showPW = !showPW;
+    console.log(showPW);
+  }
 </script>
-<div class="container text-center">
-    <h1>Welcome to {websiteName}</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<div class="fullHeight d-flex align-items-center flex-column justify-content-center row-gap">
+  <h1 class="text-center display-1">{websiteName}</h1>
+
+  <div class="container-sm text-center">
+    <div class="input-group mb-3 align-middle">
+      <span class="input-group-text" id="basic-addon1">@</span>
+      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+    </div>
+    <div class="input-group mb-3 align-middle">
+      <span class="input-group-text" id="basic-addon1">**</span>
+      <input type={showPW ? "text" : "password"} id="PW" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" />
+      <button class="input-group-text" id="basic-addon1" on:click={togglePW}><i class={"bi bi-eye" + (showPW ? "" : "-slash") + "-fill"}></i></button>
+    </div>
+
+    <button type="submit"><i class="bi bi-box-arrow-right"></i> Log In</button>
+  </div>
 </div>
