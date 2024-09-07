68644339ead4447cf56ee315d9d9a61597d6666c
Eni Adebisi <eniadebisi@gmail.com>
2024-09-06 21:48:16 -0400

    Finalized reservation page + update data via server api call instead of onload of page (Save resources ?)

--- src/routes/(authenticated)/reservations/+page.svelte
+++ src/routes/(authenticated)/reservations/+page.svelte
@@ -1,28 +1,53 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import { sampleLocations, sampleRooms, sampleReservations } from "$lib/assets/sample";
-  import { dateFormat } from "$lib/helpers";
+  import dayjs from "dayjs";
+  import AdvancedFormat from "dayjs/plugin/advancedFormat";
+  import { goto } from "$app/navigation";
+  dayjs.extend(AdvancedFormat);
   export let data: PageData;
 
-  let users = [];
-  let date = data.date ? data.date : new Date()
-  users.push(data.user);
+  let date = data.date ? new Date(data.date) : new Date();
+  let staticDate = dayjs(date).format("YYYY-MM-DD");
+
+  async function updateReserv(nDate: Date) {
+    const response = await fetch("/reservations/api/", {
+      method: "POST",
+      body: JSON.stringify({ nDate }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const { reservations: resv } = await response.json();
+
+    reservations = resv;
+    // console.log(reservations);
+    // console.log(resv);
+  }
+  let reservations = data.reservations;
 </script>
 
 <div class="px-4 py-1 mt-1 text-center d-flex flex-column align-items-center">
-  <h1 class="mb-3">Reservations for {dateFormat(date, "ddd, MMM Do")}</h1>
+  <h1 class="mb-3">Reservations for {dayjs(staticDate).format("ddd, MMM Do")}</h1>
 
   <div class="container text-center mb-2">
     <div class="row align-items-start">
       <div class="col">
-        <input type="date" name="date" id="date" />
-        <button type="button"> Go to date </button>
+        <input type="date" name="date" id="date" bind:value={date} />
+        <button
+          type="button"
+          on:click={() => {
+            updateReserv(date);
+            staticDate = dayjs(date).format("YYYY-MM-DD");
+          }}
+        >
+          Go to date
+        </button>
       </div>
       <div class="col">
-        <button type="button"> New Reservation </button>
-      </div>
-      <div class="col">
-        <button type="button"> New Location </button>
+        <a href="reservations/new">
+          <button type="button"> New Reservation </button>
+        </a>
       </div>
     </div>
   </div>
@@ -31,10 +56,10 @@
     <div class="d-flex overflow-x-hidden mt-3">
       <div class="rowStart">
         <div class="lCell Locations">Locations</div>
-        {#each sampleLocations as loc}
+        {#each data.locations as loc}
           <div class="lCell locHeaderColor">{loc.name}</div>
 
-          {#each sampleRooms.filter((room) => room.location === loc.id) as room}
+          {#each data.rooms.filter((room) => room.locationId === loc.id) as room}
             <div class="lCell roomHeaderColor">
               {room.name}
             </div>
@@ -47,19 +72,19 @@
             <div class="rCell time-header">{i + 6}:00</div>
           {/each}
         </div>
-        {#each sampleLocations as loc}
+        {#each data.locations as loc}
           <div class="tableRow">
             {#each Array(18) as _, i}
               <div class="rCell locRCell"></div>
             {/each}
           </div>
-          {#each sampleRooms.filter((room) => room.location === loc.id) as room}
+          {#each data.rooms.filter((room) => room.locationId === loc.id) as room}
             <div class="tableRow">
               {#each Array(18) as _, i}
                 <div class="rCell"></div>
               {/each}
-              {#each sampleReservations.filter((resv) => resv.roomId === room.id) as resv}
-                <div class="reservation" style="width: {85 * resv.length}px;margin-left: {85 * parseInt(dateFormat(resv.startTime, 'H')) - 6 * 85}px;">Here</div>
+              {#each reservations.filter((resv) => resv.roomId === room.id) as resv}
+                <div class="reservation" style="width: {85 * resv.length}px;margin-left: {85 * dayjs(resv.startTime).hour() - 6 * 85}px;">Here @ {dayjs(resv.startTime).hour()}</div>
               {/each}
             </div>
           {/each}
@@ -125,6 +150,7 @@
     text-align: center;
     padding: 5px;
     border-radius: 5px;
+    height: 40px;
   }
   .time-header {
     position: sticky;
