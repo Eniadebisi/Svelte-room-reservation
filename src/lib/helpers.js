2a9c2ab2a95a84f82a2fca239cbf3f992f3ae2d0
Eni Adebisi <eniadebisi@gmail.com>
2024-09-06 21:45:38 -0400

    Deleted helper and added user role definitions to settings.

--- src/lib/helpers.js
+++
@@ -1,7 +0,0 @@
-import dayjs from "dayjs";
-import advancedFormat from "dayjs/plugin/advancedFormat.js";
-dayjs.extend(advancedFormat);
-import utc from "dayjs/plugin/utc";
-dayjs.extend(utc);
-
-export function dateFormat(dateVal, format) {return dayjs.utc(dateVal).local().format(format)}
