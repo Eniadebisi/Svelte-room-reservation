666571f47d404128f8833d9619f9c7eaec1743c7
Eni Adebisi <eniadebisi@gmail.com>
2024-09-23 10:47:10 -0400

    Try catch handling added to 'auth.js'

--- src/auth.js
+++ src/auth.js
@@ -4,38 +4,42 @@ import { JWT_ACCESS_SECRET } from "$env/static/private";
 
 export const handle = async ({ event, resolve }) => {
   const { cookies, locals } = event;
-  const authCookie = cookies.get("AuthorizationToken");
+  try {
+    const authCookie = cookies.get("AuthorizationToken");
+    if (authCookie && authCookie.startsWith("Bearer ")) {
+      const token = authCookie.split(" ")[1];
+      try {
+        const jwtUser = jwt.verify(token, JWT_ACCESS_SECRET);
+        if (typeof jwtUser === "string") {
+          throw new Error("Invalid JWT token or secret key");
+        }
 
-  if (authCookie) {
-    // Remove Bearer prefix
-    const token = authCookie.split(" ")[1];
-    try {
-      const jwtUser = jwt.verify(token, JWT_ACCESS_SECRET);
-      if (typeof jwtUser === "string") {
-        throw new Error("Something went wrong");
+        const user = await prisma.user.findUnique({
+          where: {
+            id: jwtUser.id,
+          },
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        const sessionUser = {
+          id: user.id,
+          email: user.email,
+          role: user.role,
+          name: user.name,
+        };
+
+        locals.user = sessionUser;
+      } catch (error) {
+        console.error(`Error occurred: ${error.message}`);
+        console.error(`Stack trace: ${error.stack}`);
       }
-
-      const user = await prisma.user.findUnique({
-        where: {
-          id: jwtUser.id,
-        },
-      });
-
-      if (!user) {
-        throw new Error("User not found");
-      }
-
-      const sessionUser = {
-        id: user.id,
-        email: user.email,
-        role: user.role,
-        name: user.name,
-      };
-
-      locals.user = sessionUser;
-    } catch (error) {
-      console.error(error);
     }
+  } catch (error) {
+    console.error(`Error occurred during cookie retrieval: ${error.message}`);
+    console.error(`Stack trace: ${error.stack}`);
   }
 
   return await resolve(event);
